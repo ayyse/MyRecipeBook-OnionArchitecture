83518f2cd4b607b89bc689fe2be0507fc31e90cc
Ayşe Gümrükçü <aaysegumrukcu@gmail.com>
2026-01-08 22:05:43 +0300

    feat(admin): update category list using PrimeNG TreeTable

--- my-recipe-app/src/app/components/admin-components/categories/admin-category-component/admin-category-component.ts
+++ my-recipe-app/src/app/components/admin-components/categories/admin-category-component/admin-category-component.ts
@@ -1,47 +1,50 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Client } from '../../../../api/api';
 import { CommonModule } from '@angular/common';
+import { ButtonModule } from 'primeng/button';
+import { TreeTableModule } from 'primeng/treetable';
+import { TreeNode } from 'primeng/api';
 
 @Component({
   selector: 'app-admin-category-component',
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonModule, TreeTableModule],
   templateUrl: './admin-category-component.html',
   styleUrl: './admin-category-component.css',
 })
 export class AdminCategoryComponent implements OnInit {
-  categories: any[] = [];
-    expandedIds = new Set<number>();
+  categories: TreeNode[] | null = null;
 
-
-  constructor(private client: Client, private cdr: ChangeDetectorRef) { }
+  constructor(private client: Client) { }
 
   ngOnInit() {
-    this.getCategories();
+    this.getParentCategories();
   }
 
-  getCategories() {
+  getParentCategories() {
     this.client.parentCategories().subscribe(res => {
-      this.categories = res;
-      this.cdr.detectChanges();
+      this.categories = [...this.mapToTreeNode(res)];
       console.log(this.categories)
     });
   }
 
-  delete(id: string) {
-    if (!confirm('Bu kategoriyi silmek istiyor musun?')) return;
-
-    this.client.categoryDELETE(id).subscribe(() => {
-      this.categories = this.categories.filter(x => x.id !== id);
-    });
+  mapToTreeNode(categories: any[]): TreeNode[] {
+    return categories.map(c => ({
+      data: {
+        id: c.id,
+        name: c.name,
+        description: c.description
+      },
+      children: c.subCategories?.length
+        ? this.mapToTreeNode(c.subCategories)
+        : []
+    }));
   }
 
-  toggle(id: number) {
-    this.expandedIds.has(id)
-      ? this.expandedIds.delete(id)
-      : this.expandedIds.add(id);
-  }
+  // delete(id: string) {
+  //   if (!confirm('Bu kategoriyi silmek istiyor musun?')) return;
 
-  isExpanded(id: number): boolean {
-    return this.expandedIds.has(id);
-  }
+  //   this.client.categoryDELETE(id).subscribe(() => {
+  //     this.categories = this.categories.filter(x => x.id !== id);
+  //   });
+  // }
 }
